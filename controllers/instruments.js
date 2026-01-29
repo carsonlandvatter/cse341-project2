1818b2c02622c4abde649e6b99f0a703441e5b6b
carsonlandvatter <landvattercarson@gmail.com>
2026-01-28 18:18:16 -0700

    added error handlin for each route

--- controllers/instruments.js
+++ controllers/instruments.js
@@ -3,59 +3,92 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllInstruments = async (req, res) => {
     //#swagger.tags=['Instruments']
-    const result = await mongodb.getDatabase().db().collection('instruments').find();
-    result.toArray().then((instruments) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(instruments);
-    })
+   try {
+           const result = await mongodb
+           .getDatabase()
+           .db()
+           .collection('instruments')
+           .find()
+           .toArray();
+   
+           if (result.length === 0) {
+               return res.status(400).json({error: 'Instruments not found'})
+           }
+   
+           res.status(200).json(result);
+
+       } catch (err) {
+           res.status(500).json({ error: 'Server error'})
+       }
 };
 
 const getSingleInstrument = async (req, res) => {
     //#swagger.tags=['Instruments']
-    const instrumentsId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('instruments').find({ _id: instrumentsId });
-    result.toArray().then((instruments) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(instruments[0]);
-    })
+    try {
+            const instrumentsId = new ObjectId(req.params.id);
+            const result = await mongodb
+            .getDatabase()
+            .db()
+            .collection('instruments')
+            .find({ _id: instrumentsId })
+            .toArray()
+    
+            if (result.length === 0) {
+                return res.status(400).json({error: 'Instrument not found'})
+            }
+    
+            res.status(200).json(result)
+    
+        } catch (err) {
+            res.status(500).json({ error: 'Server error'})
+        }
 };
 
 const updateInstrument = async (req, res) => {
     //#swagger.tags=['Instruments']
-    const instrumentId = new ObjectId(req.params.id);
-    const instrument = {
+    try {
+        const instrumentId = new ObjectId(req.params.id);
+        const instrument = {
         instrument: req.body.instrument,
         type: req.body.type
-    }
-    const response = await mongodb.getDatabase().db().collection('instruments').replaceOne({ _id: instrumentId }, instrument);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
+        }
+        const response = await mongodb.getDatabase().db().collection('instruments').replaceOne({ _id: instrumentId }, instrument);
+
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+    } 
+    } catch (err) {
         res.status(500).json(response.error || 'Some error occurred while updating the instrument.');
     }
 }
 
 const createInstrument = async (req, res) => {
     //#swagger.tags=['Instruments']
-    const instrument = {
+    try {
+        const instrument = {
         instrument: req.body.instrument,
         type: req.body.type
-    }
-    const response = await mongodb.getDatabase().db().collection('instruments').insertOne(instrument);
-    if (response.acknowledged > 0) {
-        res.status(204).send();
-    } else {
+        }
+        const response = await mongodb.getDatabase().db().collection('instruments').insertOne(instrument);
+        if (response.acknowledged > 0) {
+            res.status(204).send();
+        } 
+
+    } catch (err) {
         res.status(500).json(response.error || 'Some error occurred while creating the instrument.');
     }
 }
 
 const deleteInstrument = async (req, res) => {
     //#swagger.tags=['Instruments']
-    const instrumentId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('instruments').deleteOne({ _id: instrumentId });
-    if (response.deletedCount > 0) {
+    try {
+        const instrumentId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('instruments').deleteOne({ _id: instrumentId });
+
+        if (response.deletedCount > 0) {
         res.status(204).send();
-    } else {
+        } 
+    } catch (err) {
         res.status(500).json(response.error || 'Some error occurred while deleting the instrument.');
     }
 }
--- controllers/sports.js
+++ controllers/sports.js
@@ -3,46 +3,75 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllSports = async (req, res) => {
     //#swagger.tags=['Sports']
-    const result = await mongodb.getDatabase().db().collection('sports').find();
-    result.toArray().then((sports) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(sports);
-    })
+    try {
+        const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('sports')
+        .find()
+        .toArray();
+
+        if (result.length === 0) {
+            return res.status(400).json({error: 'Sports not found'})
+        }
+
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json({ error: 'Server error'})
+    }
 };
 
 const getSingleSport = async (req, res) => {
     //#swagger.tags=['Sports']
-    const sportsId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('sports').find({ _id: sportsId });
-    result.toArray().then((sports) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(sports[0]);
-    })
+    try {
+        const sportsId = new ObjectId(req.params.id);
+        const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('sports')
+        .find({ _id: sportsId })
+        .toArray()
+
+        if (result.length === 0) {
+            return res.status(400).json({error: 'Sport not found'})
+        }
+
+        res.status(200).json(result)
+
+    } catch (err) {
+        res.status(500).json({ error: 'Server error'})
+    }
+    
 };
 
 const updateSport = async (req, res) => {
     //#swagger.tags=['Sports']
-    const sportsId = new ObjectId(req.params.id);
-    const sport = {
-        sport: req.body.sport,
-        type: req.body.type,
-        equipment: req.body.equipment,
-        exercise: req.body.exercise,
-        location: req.body.location,
-        league: req.body.league,
-        player: req.body.player
-    }
-    const response = await mongodb.getDatabase().db().collection('sports').replaceOne({ _id: sportsId }, sport);
-    if (response.modifiedCount > 0) {
+    try {
+        const sportsId = new ObjectId(req.params.id);
+        const sport = {
+            sport: req.body.sport,
+            type: req.body.type,
+            equipment: req.body.equipment,
+            exercise: req.body.exercise,
+            location: req.body.location,
+            league: req.body.league,
+            player: req.body.player
+        }
+        const response = await mongodb.getDatabase().db().collection('sports').replaceOne({ _id: sportsId }, sport);
+
+        if (response.modifiedCount > 0) {
         res.status(204).send();
-    } else {
+        }
+
+    } catch (err) {
         res.status(500).json(response.error || 'Some error occurred while updating the sport.');
     }
 }
 
 const createSport = async (req, res) => {
     //#swagger.tags=['Sports']
-    const sport = {
+    try {
+        const sport = {
         sport: req.body.sport,
         type: req.body.type,
         equipment: req.body.equipment,
@@ -50,22 +79,29 @@ const createSport = async (req, res) => {
         location: req.body.location,
         league: req.body.league,
         player: req.body.player
-    }
-    const response = await mongodb.getDatabase().db().collection('sports').insertOne(sport);
-    if (response.acknowledged > 0) {
-        res.status(204).send();
-    } else {
+        }
+
+        const response = await mongodb.getDatabase().db().collection('sports').insertOne(sport);
+
+        if (response.acknowledged > 0) {
+            res.status(204).send();
+        }
+    } catch (err) {
         res.status(500).json(response.error || 'Some error occurred while creating the sport.');
     }
 }
 
 const deleteSport = async (req, res) => {
     //#swagger.tags=['Sports']
-    const sportsId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('sports').deleteOne({ _id: sportsId });
-    if (response.deletedCount > 0) {
+    try {
+        const sportsId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('sports').deleteOne({ _id: sportsId });
+        
+        if (response.deletedCount > 0) {
         res.status(204).send();
-    } else {
+        }
+
+    } catch (err) {
         res.status(500).json(response.error || 'Some error occurred while deleting the sport.');
     }
 }
